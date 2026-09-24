1bbb47b11298391ee7d56358b1886fce375b39cf
Raghul <raghulsaravanakumar55@gmail.com>
2026-09-24 09:48:14 +0530

    update factorial implementation

--- factorial.py
+++ factorial.py
@@ -1,6 +1,8 @@
 def factorial(n):
     if n<0:
-        raise ValueError("Negative number")
+        raise ValueError("Factorial Can't be calulated for Negative number")
+    if n==0 or n==1:
+        return 1
     result = 1
     for i in range(2, n+1):
         result *= i
